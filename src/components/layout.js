1344799664fecbf5b950b40ef25b3cc907fb1f16
Chris Balin <chrisbalin@users.noreply.github.com>
2021-03-08 16:18:39 -0500

    Setup Gatsby site

---
+++ src/components/layout.js
@@ -0,0 +1,10 @@
+import React from "react"
+export default function Layout({ children }) {
+  return (
+    <div class="flex">
+      <div class="card">
+      </div>
+      <h2>Codeless Automation</h2>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
 import React from "react"
+import Layout from "../components/layout"
+
+import "fontsource-karla/300.css"
+import "fontsource-karla/500.css"
+
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <h1 role="img" aria-label="Waving Emoji">👋</h1>
+      <p>Hi, I'm Chris Balin, a product designer who loves solving real problems for real people.</p>
+      <p>I'm currently building no-code software testing tools at <a href="https://www.applause.com/">Applause</a>. Previously, I worked on e-commerce analytics at <a href="https://www.powerreviews.com/">PowerReviews</a> and 3D design tools for makers at <a href="https://www.inventables.com/">Inventables</a>.</p>
+      <p>Get in touch at cbalin (at) gmail.com.</p>
+    </div>
+  );
 }
