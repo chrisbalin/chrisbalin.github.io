7b7895938a1708a9927664045cec132450efe04f
Chris Balin <chrisbalin@users.noreply.github.com>
2021-03-11 17:59:09 -0500

    Portfolio magic

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import Layout from "../components/layout"
 
 import "fontsource-karla/300.css"
 import "fontsource-karla/500.css"
@@ -11,7 +10,7 @@ export default function Home() {
       <h1 role="img" aria-label="Waving Emoji">👋</h1>
       <p>Hi, I'm Chris Balin, a product designer who loves solving real problems for real people.</p>
       <p>I'm currently building no-code software testing tools at <a href="https://www.applause.com/">Applause</a>. Previously, I worked on e-commerce analytics at <a href="https://www.powerreviews.com/">PowerReviews</a> and 3D design tools for makers at <a href="https://www.inventables.com/">Inventables</a>.</p>
-      <p>Get in touch at cbalin (at) gmail.com.</p>
+      <p>Get in touch at hello@chrisbalin.com.</p>
     </div>
   );
 }
