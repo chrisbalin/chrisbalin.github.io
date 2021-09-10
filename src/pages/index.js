ae2748e41c23ae61d1d6b3d582f7ee3b7558f9f6
Chris Balin <chrisbalin@users.noreply.github.com>
2021-09-09 20:46:03 -0400

    Update to Dispatch

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,8 @@ export default function Home() {
     <div class="home">
       <h1 role="img" aria-label="Waving Emoji">👋</h1>
       <p>Hi, I'm Chris Balin, a product designer who loves solving real problems for real people.</p>
-      <p>I'm currently building no-code software testing tools at <a href="https://www.applause.com/">Applause</a>. Previously, I worked on e-commerce analytics at <a href="https://www.powerreviews.com/">PowerReviews</a> and 3D design tools for makers at <a href="https://www.inventables.com/">Inventables</a>.</p>
+      <p>I'm currently at <a href="https://www.dispatchhealth.com/">DispatchHealth</a>, helping to deliver health care to people in the comfort of their own home.</p>
+      <p>Previously, I worked on software testing tools at <a href="https://www.applause.com/">Applause</a>, e-commerce analytics at <a href="https://www.powerreviews.com/">PowerReviews</a>, and 3D design tools for makers at <a href="https://www.inventables.com/">Inventables</a>.</p>
       <p>Get in touch at hello@chrisbalin.com.</p>
     </div>
   );
