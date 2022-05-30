afbc3679988133ff31e4f5955022a6d0237d745e
Chris Balin <chrisbalin@users.noreply.github.com>
2022-05-30 13:43:59 -0700

    updated text in intro

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div class="home">
       <h1 role="img" aria-label="Waving Emoji">👋</h1>
-      <p>Hi, I'm Chris Balin, a product designer who loves solving real problems for real people.</p>
+      <p>Hello, I'm Chris Balin, a product designer who loves solving real problems for real people.</p>
       <p>I'm currently at <a href="https://www.dispatchhealth.com/">DispatchHealth</a>, helping to deliver health care to people in the comfort of their own home.</p>
       <p>Previously, I worked on software testing tools at <a href="https://www.applause.com/">Applause</a>, e-commerce analytics at <a href="https://www.powerreviews.com/">PowerReviews</a>, and 3D design tools for makers at <a href="https://www.inventables.com/">Inventables</a>.</p>
       <p>Get in touch at hello@chrisbalin.com.</p>
